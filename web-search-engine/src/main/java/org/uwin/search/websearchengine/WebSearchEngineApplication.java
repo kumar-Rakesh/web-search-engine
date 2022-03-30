ac2a2361bf155116fb9d1fb4460056e050fcfb3a
Rakesh Kumar <kumar4a@uwindsor.ca>
2022-03-30 17:42:10 -0400

    Init Setup

---
+++ web-search-engine/src/main/java/org/uwin/search/websearchengine/WebSearchEngineApplication.java
@@ -0,0 +1,13 @@
+package org.uwin.search.websearchengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSearchEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSearchEngineApplication.class, args);
+	}
+
+}
---
+++ web-search-engine/src/test/java/org/uwin/search/websearchengine/WebSearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uwin.search.websearchengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSearchEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
